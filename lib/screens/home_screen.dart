af2df058ed1b366a5a25a69c5136b9316f94cc3b
a-tiny-developer <atinyflame@gmail.com>
2022-02-18 21:30:56 -0600

    changed app name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app_flutter/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,9 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
+      body: AuthBackground(),
     );
   }
 }
